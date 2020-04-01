b77dd8cd0e1bee6285e99c29ba648e852697b040
sanjaymajhi <1sanjaymajhi@gmail.com>
2020-04-02 00:50:28 +0530

    installed package cors

--- app.js
+++ app.js
@@ -44,6 +44,9 @@ app.use(function(err, req, res, next) {
   res.render("error");
 });
 
+var cors = require("cors");
+app.use(cors());
+
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
