7ee60d6eade5ee54dc23aaafa483b1227c43f498
sanjaymajhi <1sanjaymajhi@gmail.com>
2020-04-02 11:38:15 +0530

    fixing cors

--- controller/extapiController.js
+++ controller/extapiController.js
@@ -23,3 +23,42 @@ exports.refundFetch = (req, res) => {
   };
   Request(options).then(data => res.json({ ...data }));
 };
+
+exports.ticketStatus = (req, res) => {
+  var options = {
+    uri:
+      "https://api.razorpay.com/v1/invoices?type=link&payment_id=" +
+      req.query.payment_id,
+    headers: {
+      authorization:
+        "Basic cnpwX3Rlc3Rfa0hiVWVmN1diTVJJQ3M6dUF1UHRRRExBbXN3aEZHb0NDYklCdWZz"
+    },
+    json: true
+  };
+  Request(options).then(data => res.json({ ...data }));
+};
+
+exports.ticket_book_get = (req, res) => {
+  var options = {
+    uri: "https://api.razorpay.com/v1/invoices/" + req.params.id,
+    headers: {
+      authorization:
+        "Basic cnpwX3Rlc3Rfa0hiVWVmN1diTVJJQ3M6dUF1UHRRRExBbXN3aEZHb0NDYklCdWZz"
+    },
+    json: true
+  };
+  Request(options).then(data => res.json({ ...data }));
+};
+exports.ticket_book_post = (req, res) => {
+  var options = {
+    method: "POST",
+    uri: "https://api.razorpay.com/v1/invoices/",
+    body: { ...req.body },
+    headers: {
+      authorization:
+        "Basic cnpwX3Rlc3Rfa0hiVWVmN1diTVJJQ3M6dUF1UHRRRExBbXN3aEZHb0NDYklCdWZz"
+    },
+    json: true
+  };
+  Request(options).then(data => res.json({ ...data }));
+};
--- routes/extapi.js
+++ routes/extapi.js
@@ -6,4 +6,9 @@ var extapiController = require("../controller/extapiController");
 Router.get("/payments/:id", extapiController.paymentFetch);
 Router.get("/payments/:id/refunds", extapiController.refundFetch);
 
+Router.get("/invoices/", extapiController.ticketStatus);
+
+Router.get("/invoices/:id", extapiController.ticket_book_get);
+Router.post("/invoices/", extapiController.ticket_book_post);
+
 module.exports = Router;
