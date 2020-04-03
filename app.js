c1264ba648fda9bbd15c7ad6b4ac6a9873855e52
sanjaymajhi <1sanjaymajhi@gmail.com>
2020-04-03 13:10:55 +0530

    Ready to rock

--- app.js
+++ app.js
@@ -50,6 +50,9 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+}
 app.get("*", (request, response) => {
   response.sendFile(path.join(__dirname, "build", "index.html"));
 });
