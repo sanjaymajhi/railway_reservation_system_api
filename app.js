b7a13d7ae41e8b8c22a2b276f13e3cfdb58bded8
sanjaymajhi <1sanjaymajhi@gmail.com>
2020-04-03 13:12:27 +0530

    Ready to rock

--- app.js
+++ app.js
@@ -50,13 +50,6 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
-}
-app.get("*", (request, response) => {
-  response.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
 var compression = require("compression");
 app.use(compression());
 
